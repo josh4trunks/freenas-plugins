b1657f920dc94c1099d0f22085d88cb2aa0985e8
William Grzybowski <wg@FreeBSD.org>
2013-09-18 15:29:44 -0300

    Remove typo leftover and needless Loadmodule

--- plugins/owncloud/scripts/post-install.sh
+++ plugins/owncloud/scripts/post-install.sh
@@ -8,8 +8,6 @@ owncloud_pbi_path=/usr/pbi/owncloud-$(uname -m)
 ${owncloud_pbi_path}/bin/python ${owncloud_pbi_path}/owncloudUI/manage.py syncdb --migrate --noinput
 
 cat << __EOF__ > ${owncloud_pbi_path}/etc/apache22/Includes/owncloud.conf
-LoadModule php5_module libexec/apache22/libphp5.so
-
 Alias / ${owncloud_pbi_path}/www/owncloud/
 AcceptPathInfo On
 <Directory ${owncloud_pbi_path}/www/owncloud>
@@ -17,5 +15,4 @@ AcceptPathInfo On
     Order Allow,Deny
     Allow from all
 </Directory>
-asdasd
 __EOF__
