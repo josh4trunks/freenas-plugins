a41bfc716597c64c8f60b64fe692bae31e463a2f
William Grzybowski <wg@FreeBSD.org>
2013-09-18 13:40:03 -0300

    Install owncloud configuration for apache

--- plugins/owncloud/scripts/post-install.sh
+++ plugins/owncloud/scripts/post-install.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
 #########################################
 
-owncloud_pbi_path=/usr/pbi/owncloud-$(uname -m)/
+owncloud_pbi_path=/usr/pbi/owncloud-$(uname -m)
 
 ${owncloud_pbi_path}/bin/python ${owncloud_pbi_path}/owncloudUI/manage.py syncdb --migrate --noinput
+
+cat << __EOF__ > ${owncloud_pbi_path}/etc/apache22/Includes/owncloud.conf
+LoadModule php5_module libexec/apache22/libphp5.so
+
+Alias / ${owncloud_pbi_path}/www/owncloud/
+AcceptPathInfo On
+<Directory ${owncloud_pbi_path}/www/owncloud>
+    AllowOverride All
+    Order Allow,Deny
+    Allow from all
+</Directory>
+asdasd
+__EOF__
