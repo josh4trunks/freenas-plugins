c1509cde43336e011261f538fbf032fecf6caf65
William Grzybowski <wg@FreeBSD.org>
2013-09-24 17:33:21 -0300

    Hack for linux libraries

--- plugins/crashplan/scripts/post-install.sh
+++ plugins/crashplan/scripts/post-install.sh
@@ -5,4 +5,7 @@ crashplan_pbi_path=/usr/pbi/crashplan-$(uname -m)
 
 /bin/cp ${owncloud_pbi_path}/etc/rc.d/crashplan /usr/local/etc/rc.d/
 
+mkdir -p /compat/linux
+ln -sf ${owncloud_pbi_path}/linuxlib /compat/linux/lib
+
 ${crashplan_pbi_path}/bin/python ${crashplan_pbi_path}/crashplanUI/manage.py syncdb --migrate --noinput
