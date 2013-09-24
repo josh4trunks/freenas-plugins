fce289f964c9a41e08dec00228c00634ef4acfa1
William Grzybowski <wg@FreeBSD.org>
2013-09-24 15:48:45 -0300

    Copy crashplan rc.d

--- plugins/crashplan/scripts/post-install.sh
+++ plugins/crashplan/scripts/post-install.sh
@@ -3,4 +3,6 @@
 
 crashplan_pbi_path=/usr/pbi/crashplan-$(uname -m)
 
+/bin/cp ${owncloud_pbi_path}/etc/rc.d/crashplan /usr/local/etc/rc.d/
+
 ${crashplan_pbi_path}/bin/python ${crashplan_pbi_path}/crashplanUI/manage.py syncdb --migrate --noinput
