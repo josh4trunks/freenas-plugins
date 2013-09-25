d0f0c710aabc68e8f29651d70c2ef91f6a992b2d
William Grzybowski <wg@FreeBSD.org>
2013-09-25 08:54:49 -0300

    Use correct pbi path and use patchelf for java

--- plugins/crashplan/scripts/post-install.sh
+++ plugins/crashplan/scripts/post-install.sh
@@ -3,9 +3,14 @@
 
 crashplan_pbi_path=/usr/pbi/crashplan-$(uname -m)
 
-/bin/cp ${owncloud_pbi_path}/etc/rc.d/crashplan /usr/local/etc/rc.d/
+/bin/cp ${crashplan_pbi_path}/etc/rc.d/crashplan /usr/local/etc/rc.d/
 
 mkdir -p /compat/linux
-ln -sf ${owncloud_pbi_path}/linuxlib /compat/linux/lib
+ln -sf ${crashplan_pbi_path}/linuxlib /compat/linux/lib
+
+#For some reason the default relative RPATH doesnt work in java bin
+${crashplan_pbi_path}/bin/patchelf \
+	--set-rpath ${crashplan_pbi_path}/linux-sun-jre1.6.0/lib/i386/jli \
+	${crashplan_pbi_path}/linux-sun-jre1.6.0/bin/java
 
 ${crashplan_pbi_path}/bin/python ${crashplan_pbi_path}/crashplanUI/manage.py syncdb --migrate --noinput
