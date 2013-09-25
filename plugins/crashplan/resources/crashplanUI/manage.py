6d4f2c05d57473be3b41230724e44e8762d6756a
William Grzybowski <wg@FreeBSD.org>
2013-09-25 09:08:19 -0300

    Fix crashplan path in the UI

--- plugins/crashplan/resources/crashplanUI/manage.py
+++ plugins/crashplan/resources/crashplanUI/manage.py
@@ -9,16 +9,16 @@ python_major = sys.version_info.major
 python_minor = sys.version_info.minor
 python = "python%d.%d" % (python_major, python_minor)
 
-OWNCLOUD_PATH = "/usr/pbi/owncloud-%s" % arch
-OWNCLOUD_UI = os.path.join(OWNCLOUD_PATH, "owncloudUI")
-PYTHON_SITE_PACKAGES = os.path.join(OWNCLOUD_PATH,
+PBI_PATH = "/usr/pbi/crashplan-%s" % arch
+PBI_UI = os.path.join(PBI_PATH, "crashplanUI")
+PYTHON_SITE_PACKAGES = os.path.join(PBI_PATH,
     "lib/%s/site-packages" % python)
 
 sys.path.append(PYTHON_SITE_PACKAGES)
-sys.path.append(OWNCLOUD_PATH)
-sys.path.append(OWNCLOUD_UI)
+sys.path.append(PBI_PATH)
+sys.path.append(PBI_UI)
 
-os.environ["DJANGO_SETTINGS_MODULE"] = "owncloudUI.settings"
+os.environ["DJANGO_SETTINGS_MODULE"] = "crashplanUI.settings"
 
 from django.core.management import execute_from_command_line
 
--- plugins/crashplan/resources/crashplanUI/urls.py
+++ plugins/crashplan/resources/crashplanUI/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-     url(r'^plugins/owncloud/(?P<plugin_id>\d+)/', include('owncloudUI.freenas.urls')),
+     url(r'^plugins/crashplan/(?P<plugin_id>\d+)/', include('crashplanUI.freenas.urls')),
 )
