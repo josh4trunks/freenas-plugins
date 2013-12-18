26cb2956b996086afcfab0b69768893a0452655b
John Hixson <john@ixsystems.com>
2013-12-18 00:32:46 -0800

    Update plex plugin to fix subtitles as well as open in new window
    Ticket: #3699

--- plugins/plexmediaserver/scripts/post-install.sh
+++ plugins/plexmediaserver/scripts/post-install.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 #########################################
 
+plexmediaserver_resources="/usr/local/share/plexmediaserver/Resources"
 plexmediaserver_pbi_path=/usr/pbi/plexmediaserver-$(uname -m)/
 plexmediaserver_plexdata="${plexmediaserver_pbi_path}/plexdata"
 plexmediaserver_pms="${plexmediaserver_plexdata}/Plex Media Server"
 plexmediaserver_media="${plexmediaserver_pms}/Media"
 plexmediaserver_databases="${plexmediaserver_pms}/Plug-in Support/Databases"
+
+plexmediaserver_library="${plexmediaserver_pms}/Library"
+plexmediaserver_fonts="${plexmediaserver_library}/Fonts"
 
 pw group add plex
 pw user add plex -g plex -d "${plexmediaserver_pbi_path}"
@@ -15,6 +19,10 @@ mkdir -p "${plexmediaserver_media}/TV Shows"
 mkdir -p "${plexmediaserver_media}/Music"
 mkdir -p "${plexmediaserver_media}/Photos"
 mkdir -p "${plexmediaserver_media}/Home Movies"
+
+mkdir -p "${plexmediaserver_fonts}"
+cp "${plexmediaserver_resources}/DejaVuUniversal.ttf" "${plexmediaserver_fonts}/Unicode.ttf" 
+cp "${plexmediaserver_resources}/DejaVuUniversal.ttf" "${plexmediaserver_fonts}/Arial.ttf"
 
 ${plexmediaserver_pbi_path}/bin/python ${plexmediaserver_pbi_path}/plexmediaserverUI/manage.py syncdb --migrate --noinput
 ~
