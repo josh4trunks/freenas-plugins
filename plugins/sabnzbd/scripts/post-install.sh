d4c1f4878ad3a454b1f4842439619e66ece981af
josh4trunks <joshruehlig@gmail.com>
2014-03-26 21:24:29 -0700

    Update SABnzbd to latest, use /var/db instead

--- plugins/sabnzbd/scripts/post-install.sh
+++ plugins/sabnzbd/scripts/post-install.sh
@@ -3,3 +3,8 @@
 sabnzbd_pbi_path=/usr/pbi/sabnzbd-$(uname -m)
 
 ${sabnzbd_pbi_path}/bin/python2.7 ${sabnzbd_pbi_path}/sabnzbdUI/manage.py syncdb --migrate --noinput
+
+####
+mkdir -p /var/db/sabnzbd
+chown -R media:media /var/db/sabnzbd
+####
