1856e947deb35e27f38dbf49d260467df5f4dae9
josh4trunks <joshruehlig@gmail.com>
2014-04-30 07:35:43 -0700

    Use /var/db fopr htpc-manager

--- plugins/htpc-manager/scripts/post-install.sh
+++ plugins/htpc-manager/scripts/post-install.sh
@@ -3,3 +3,8 @@
 htpc_manager_pbi_path=/usr/pbi/htpc-manager-$(uname -m)
 
 ${htpc_manager_pbi_path}/bin/python2.7 ${htpc_manager_pbi_path}/htpcmanagerUI/manage.py syncdb --migrate --noinput
+
+####
+mkdir -p /var/db/htpc-manager
+chown -R media:media /var/db/htpc-manager
+####
