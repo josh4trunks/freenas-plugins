92378e180765704762c059d257963e11a4d5cfbd
josh4trunks <joshruehlig@gmail.com>
2014-05-18 18:23:26 -0700

    Use /var/db for lazylibrarian

--- plugins/lazylibrarian/scripts/post-install.sh
+++ plugins/lazylibrarian/scripts/post-install.sh
@@ -3,3 +3,8 @@
 lazylibrarian_pbi_path=/usr/pbi/lazylibrarian-$(uname -m)
 
 ${lazylibrarian_pbi_path}/bin/python2.7 ${lazylibrarian_pbi_path}/lazylibrarianUI/manage.py syncdb --migrate --noinput
+
+####
+mkdir -p /var/db/lazylibrarian
+chown -R media:media /var/db/lazylibrarian
+####
