50bf7212081d0bc6c30680e68fef560eb40ff4f3
josh4trunks <joshruehlig@gmail.com>
2014-05-18 18:49:03 -0700

    Remove Gamez

--- plugins/gamez/resources/gamezUI/urls.py
+++
@@ -1,5 +0,0 @@
-from django.conf.urls import patterns, include, url
-
-urlpatterns = patterns('',
-     url(r'^plugins/gamez/(?P<plugin_id>\d+)/', include('gamezUI.freenas.urls')),
-)
--- plugins/gamez/scripts/post-install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-gamez_pbi_path=/usr/pbi/gamez-$(uname -m)
-
-${gamez_pbi_path}/bin/python ${gamez_pbi_path}/gamezUI/manage.py syncdb --migrate --noinput
