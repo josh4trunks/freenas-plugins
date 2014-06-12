c246f80bb94d7bc4b59cc5ceb477248e2c5dc39b
josh4trunks <joshruehlig@gmail.com>
2014-06-11 23:35:56 -0700

    Forcestop plugins during pre-removal.

--- plugins/syncthing/scripts/pre-remove.sh
+++ plugins/syncthing/scripts/pre-remove.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 syncthing_pbi_path=/usr/pbi/syncthing-$(uname -m)
-${syncthing_pbi_path}/etc/rc.d/syncthing forcestop
+
+${syncthing_pbi_path}/etc/rc.d/syncthing forcestop 2>/dev/null || true
+
 if cmp -s ${syncthing_pbi_path}/etc/syncthing.xml.sample /var/db/syncthing/config.xml; then rm -f /var/db/syncthing/config.xml; fi
