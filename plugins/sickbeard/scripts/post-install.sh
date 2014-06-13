a8ec30b6c812a5d29e41f504715f237bcb60f4bc
josh4trunks <joshruehlig@gmail.com>
2014-06-12 22:10:21 -0700

    Fix typos from last commit

--- plugins/sickbeard/scripts/post-install.sh
+++ plugins/sickbeard/scripts/post-install.sh
@@ -3,3 +3,8 @@
 sickbeard_pbi_path=/usr/pbi/sickbeard-$(uname -m)
 
 ${sickbeard_pbi_path}/bin/python2.7 ${sickbeard_pbi_path}/sickbeardUI/manage.py syncdb --migrate --noinput
+
+####
+mkdir -p /var/db/sickbeard
+chown -R media:media /var/db/sickbeard
+####
