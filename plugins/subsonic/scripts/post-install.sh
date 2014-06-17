fedf7f728f14ca4dd83f74d30c040beb8167ef2f
josh4trunks <joshruehlig@gmail.com>
2014-06-16 23:26:32 -0700

    Add transcoding support to PBI

--- plugins/subsonic/scripts/post-install.sh
+++ plugins/subsonic/scripts/post-install.sh
@@ -5,6 +5,7 @@ subsonic_pbi_path=/usr/pbi/subsonic-$(uname -m)
 ${subsonic_pbi_path}/bin/python2.7 ${subsonic_pbi_path}/subsonicUI/manage.py syncdb --migrate --noinput
 
 ####
-mkdir -p /var/db/subsonic
+mkdir -p /var/db/subsonic/transcode
+ln -s ${subsonic_pbi_path}/bin/ffmpeg /var/db/subsonic/transcode/ffmpeg
 chown -R media:media /var/db/subsonic
 ####
--- plugins/subsonic/scripts/post-remove.sh
+++ plugins/subsonic/scripts/post-remove.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+unlink /var/db/subsonic/transcode/ffmpeg
+rmdir /var/db/subsonic/transcode
 rmdir /var/db/subsonic
