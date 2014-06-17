654ff5c5a1f5460e13047da887abdc8e4ffa2a5f
josh4trunks <joshruehlig@gmail.com>
2014-06-16 23:05:01 -0700

    Subsonic, use /var/db, @sample

--- plugins/subsonic/scripts/post-install.sh
+++ plugins/subsonic/scripts/post-install.sh
@@ -3,3 +3,8 @@
 subsonic_pbi_path=/usr/pbi/subsonic-$(uname -m)
 
 ${subsonic_pbi_path}/bin/python2.7 ${subsonic_pbi_path}/subsonicUI/manage.py syncdb --migrate --noinput
+
+####
+mkdir -p /var/db/subsonic
+chown -R media:media /var/db/subsonic
+####
