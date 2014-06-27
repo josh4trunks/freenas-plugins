8c0efe5220180b2797003cfdd50eac98806b9510
josh4trunks <joshruehlig@gmail.com>
2014-06-26 20:02:08 -0700

    Fix bug where subsonic's datadir doesn't have its permissions set

--- plugins/subsonic/scripts/post-install.sh
+++ plugins/subsonic/scripts/post-install.sh
@@ -4,5 +4,5 @@ subsonic_pbi_path=/usr/pbi/subsonic-$(uname -m)
 
 ${subsonic_pbi_path}/bin/python2.7 ${subsonic_pbi_path}/subsonicUI/manage.py syncdb --migrate --noinput
 
-install -o media -g media -d /var/db/subsonic/transcode
+install -o media -g media -d /var/db/subsonic /var/db/subsonic/transcode
 ln -s ${subsonic_pbi_path}/bin/ffmpeg /var/db/subsonic/transcode/ffmpeg
