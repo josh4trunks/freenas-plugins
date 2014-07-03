e0e2bdc1887e725754758804273fc36d65ea8223
josh4trunks <joshruehlig@gmail.com>
2014-07-02 23:58:54 -0700

    Use dedicated user for syncthing

--- plugins/syncthing/scripts/post-install.sh
+++ plugins/syncthing/scripts/post-install.sh
@@ -4,5 +4,5 @@ syncthing_pbi_path=/usr/pbi/syncthing-$(uname -m)
 
 ${syncthing_pbi_path}/bin/python2.7 ${syncthing_pbi_path}/syncthingUI/manage.py syncdb --migrate --noinput
 
-install -o syncer -g syncer -d /var/db/syncthing
-if [ ! -f /var/db/syncthing/config.xml ] ; then install -o syncer -g syncer ${syncthing_pbi_path}/etc/syncthing.xml.sample /var/db/syncthing/config.xml; fi
+install -o syncthing -g syncthing -d /var/db/syncthing
+if [ ! -f /var/db/syncthing/config.xml ] ; then install -o syncthing -g syncthing ${syncthing_pbi_path}/etc/syncthing.xml.sample /var/db/syncthing/config.xml; fi
