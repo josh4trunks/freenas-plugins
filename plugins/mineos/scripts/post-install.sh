6b7fa500c871919d24cb355f02f65d6396c53021
josh4trunks <joshruehlig@gmail.com>
2014-08-06 00:08:13 -0700

    Cleanup & update MineOS

--- plugins/mineos/scripts/post-install.sh
+++ plugins/mineos/scripts/post-install.sh
@@ -4,10 +4,6 @@ mineos_pbi_path=/usr/pbi/mineos-$(uname -m)
 
 ${mineos_pbi_path}/bin/python2.7 ${mineos_pbi_path}/mineosUI/manage.py syncdb --migrate --noinput
 
-####
-mkdir -p /usr/compat/linux/proc
-pw adduser mcserver -d /nonexistent -s /usr/sbin/nologin
-echo 'mcpass' | pw mod user mcserver -h 0
-mkdir -p /var/games/minecraft/profiles && touch /var/games/minecraft/profiles/profile.config
-chown mcserver /var/games/minecraft/profiles/profile.config
-####
+mkdir -p /usr/compat/linux/proc /var/games/minecraft/profiles
+touch /var/games/minecraft/profiles/profile.config && chown mcserver /var/games/minecraft/profiles/profile.config
+echo 'mcserver' | pw mod user mcserver -h 0
