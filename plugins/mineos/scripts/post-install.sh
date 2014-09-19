789717687489f725ce398d78035ca7c13ed664eb
josh4trunks <joshruehlig@gmail.com>
2014-09-18 21:00:28 -0700

    use install instead of touch + chmod

--- plugins/mineos/scripts/post-install.sh
+++ plugins/mineos/scripts/post-install.sh
@@ -5,5 +5,5 @@ mineos_pbi_path=/usr/pbi/mineos-$(uname -m)
 ${mineos_pbi_path}/bin/python2.7 ${mineos_pbi_path}/mineosUI/manage.py syncdb --migrate --noinput
 
 mkdir -p /usr/compat/linux/proc /var/games/minecraft/profiles
-touch /var/games/minecraft/profiles/profile.config && chown mcserver /var/games/minecraft/profiles/profile.config
+install -o mcserver /dev/null /var/games/minecraft/profiles/profile.config
 echo 'mcserver' | pw mod user mcserver -h 0
