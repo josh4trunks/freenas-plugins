0b5e71e98622fade4d591f8a82117415a6de208d
josh4trunks <joshruehlig@gmail.com>
2014-09-26 21:48:50 -0700

    cleanup subsonic link code

--- plugins/subsonic/resources/subsonicUI/freenas/views.py
+++ plugins/subsonic/resources/subsonicUI/freenas/views.py
@@ -262,11 +262,9 @@ def edit(request, plugin_id):
         jail = json.loads(server.plugins.jail.info(plugin_id))[0]['fields']
         jail_ipv4 = jail['jail_ipv4'].split('/')[0]
 	if subsonic.subsonic_ssl:
-		subsonic_scheme = "https"
+		scheme = "https"
 	else:
-		subsonic_scheme = "http"
-	subsonic_port = subsonic.subsonic_port
-	subsonic_webroot = subsonic.subsonic_context_path
+		scheme = "http"
         auth = server.plugins.is_authenticated(
             request.COOKIES.get("sessionid", "")
             )
@@ -279,9 +277,9 @@ def edit(request, plugin_id):
         return render(request, "edit.html", {
             'form': form,
             'ipv4': jail_ipv4,
-            'scheme': subsonic_scheme,
-            'port': subsonic_port,
-            'webroot': subsonic_webroot
+            'scheme': scheme,
+            'port': subsonic.subsonic_port,
+            'webroot': subsonic.subsonic_context_path
         })
 
     if not request.POST:
