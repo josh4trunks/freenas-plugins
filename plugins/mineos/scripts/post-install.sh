5882fb674021b9bd4f56aeeb3aa94418bca151de
josh4trunks <joshruehlig@gmail.com>
2014-09-29 20:31:42 -0700

    Change to group ownership

--- plugins/mineos/scripts/post-install.sh
+++ plugins/mineos/scripts/post-install.sh
@@ -5,5 +5,5 @@ mineos_pbi_path=/usr/pbi/mineos-$(uname -m)
 ${mineos_pbi_path}/bin/python2.7 ${mineos_pbi_path}/mineosUI/manage.py syncdb --migrate --noinput
 
 mkdir -p /usr/compat/linux/proc /var/games/minecraft/profiles
-install -o mcserver /dev/null /var/games/minecraft/profiles/profile.config
+install -g mcserver /dev/null /var/games/minecraft/profiles/profile.config
 echo 'mcserver' | pw mod user mcserver -h 0
