413346f5db9f5222d84c2d81eb17c3f485c01b33
josh4trunks <joshruehlig@gmail.com>
2014-11-17 22:45:08 -0800

    Finish up syncthing plugin

--- plugins/syncthing/scripts/post-install.sh
+++ plugins/syncthing/scripts/post-install.sh
@@ -3,3 +3,13 @@
 syncthing_pbi_path=/usr/pbi/syncthing-$(uname -m)
 
 ${syncthing_pbi_path}/bin/python2.7 ${syncthing_pbi_path}/syncthingUI/manage.py syncdb --migrate --noinput
+
+# If syncthing hasn't been run before have it accept remote connections
+if [ ! -f /var/db/syncthing/config.xml ]; then
+        service syncthing onestart
+        while [ ! -f /var/db/syncthing/config.xml ]; do
+                sleep 1
+        done
+        service syncthing onestop
+        sed -i '' -e 's|^\([[:space:]]*<address>\)127\.0\.0\.1\(:[0-9][0-9]*</address>\)$|\10.0.0.0\2|' /var/db/syncthing/config.xml
+fi
