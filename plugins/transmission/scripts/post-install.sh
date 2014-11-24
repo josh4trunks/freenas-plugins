f1282bdeb8a0fc239aa0c3ef2061743d71b796cf
josh4trunks <joshruehlig@gmail.com>
2014-11-23 20:30:11 -0800

    More script cleanup

--- plugins/transmission/scripts/post-install.sh
+++ plugins/transmission/scripts/post-install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #########################################
 
-transmission_pbi_path=/usr/pbi/transmission-$(uname -m)/
+transmission_pbi_path=/usr/pbi/transmission-$(uname -m)
 
 ${transmission_pbi_path}/bin/python2.7 ${transmission_pbi_path}/transmissionUI/manage.py syncdb --migrate --noinput
 
