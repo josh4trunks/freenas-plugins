464a22e9ab642e2476beb13dc4c8b2b6a97b6e97
josh4trunks <joshruehlig@gmail.com>
2014-11-24 19:21:31 -0800

    Workaround no longer needed cause 'reload' was added upstream

--- plugins/transmission/scripts/post-install.sh
+++ plugins/transmission/scripts/post-install.sh
@@ -5,7 +5,4 @@ transmission_pbi_path=/usr/pbi/transmission-$(uname -m)
 
 ${transmission_pbi_path}/bin/python2.7 ${transmission_pbi_path}/transmissionUI/manage.py syncdb --migrate --noinput
 
-sed -i '' -e '/^start_precmd=transmission_prestart$/ a\
-extra_commands="reload"' ${transmission_pbi_path}/etc/rc.d/transmission
-
 sysrc 'transmission_download_dir='
