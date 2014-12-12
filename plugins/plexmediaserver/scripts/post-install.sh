31ce7a224d69f1acbefd27dd736a97821d3bb6e6
Charlie Root <joshruehlig@gmail.com>
2014-12-11 23:05:38 -0800

    Migrate plex datadirectory to a better location

--- plugins/plexmediaserver/scripts/post-install.sh
+++ plugins/plexmediaserver/scripts/post-install.sh
@@ -6,3 +6,6 @@ plexmediaserver_pbi_path=/usr/pbi/plexmediaserver-$(uname -m)
 ${plexmediaserver_pbi_path}/bin/python2.7 ${plexmediaserver_pbi_path}/plexmediaserverUI/manage.py syncdb --migrate --noinput
 
 sysrc 'plexmediaserver_support_path=/var/db/plexdata'
+
+#Temporary Workaround
+mv ${plexmediaserver_pbi_path}/plexdata /var/db/plexdata
