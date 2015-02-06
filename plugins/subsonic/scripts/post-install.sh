cade64f0a920fed8653436b3bc541c4d1d148a56
Charlie Root <joshruehlig@gmail.com>
2015-02-06 00:19:01 -0800

    Fix Status page on Subsonic plugin. Bumb version.

--- plugins/subsonic/scripts/post-install.sh
+++ plugins/subsonic/scripts/post-install.sh
@@ -4,3 +4,18 @@
 subsonic_pbi_path=/usr/pbi/subsonic-$(uname -m)
 
 ${subsonic_pbi_path}/bin/python2.7 ${subsonic_pbi_path}/subsonicUI/manage.py syncdb --migrate --noinput
+
+# Install DejaVu fonts if they don't exist
+if [ ! -d /usr/local/lib/X11/fonts/dejavu ]; then
+	fetch "http://sourceforge.net/projects/dejavu/files/dejavu/2.34/dejavu-fonts-ttf-2.34.tar.bz2"
+	tar xjf dejavu-fonts-ttf-*.tar.bz2
+	mv dejavu-fonts-ttf-*/ttf /usr/local/lib/X11/fonts/dejavu
+	rm -r dejavu-fonts-ttf-*
+fi
+# Install fonts config if it doesn't exist
+if [ ! -f /usr/local/etc/fonts/fonts.conf ]; then
+	mkdir -p /usr/local/etc/fonts
+	mv ${subsonic_pbi_path}/fonts.conf /usr/local/etc/fonts
+else
+	rm ${subsonic_pbi_path}/fonts.conf
+fi
