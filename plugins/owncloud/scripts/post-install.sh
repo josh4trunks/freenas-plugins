74cbc52fe160e8946669a08d26eab08c40cfbbac
Charlie Root <joshruehlig@gmail.com>
2015-02-18 11:36:55 -0800

    Add cronjob and comments to owncloud post-install

--- plugins/owncloud/scripts/post-install.sh
+++ plugins/owncloud/scripts/post-install.sh
@@ -5,6 +5,7 @@ owncloud_pbi_path=/usr/pbi/owncloud-$(uname -m)
 
 ${owncloud_pbi_path}/bin/python2.7 ${owncloud_pbi_path}/owncloudUI/manage.py syncdb --migrate --noinput
 
+#Optionaly set /media as the ownCloud data-directory
 if [ ! -f "${owncloud_pbi_path}/www/owncloud/config/config.php" ]; then
 	cat << __EOF__ > ${owncloud_pbi_path}/www/owncloud/config/config.php
 	<?php
@@ -15,6 +16,12 @@ if [ ! -f "${owncloud_pbi_path}/www/owncloud/config/config.php" ]; then
 __EOF__
 fi
 
+#Create cronjob for ownCloud
+tmp=$(mktemp /tmp/tmp.XXXXXX)
+echo "*/15 * * * * ${owncloud_pbi_path}/bin/php -f ${owncloud_pbi_path}/www/owncloud/cron.php" > ${tmp}
+crontab -u www ${tmp}
+
+#Create Apache alias for owncloud
 cat << __EOF__ > ${owncloud_pbi_path}/etc/apache24/Includes/owncloud.conf
 AddType application/x-httpd-php .php
 
@@ -30,8 +37,7 @@ AcceptPathInfo On
 </Directory>
 __EOF__
 
-chown -R www:www ${owncloud_pbi_path}/www/owncloud \
-	/media
+chown -R www:www ${owncloud_pbi_path}/www/owncloud media
 
 
 # Generate SSL certificate
@@ -41,7 +47,6 @@ if [ ! -f "${owncloud_pbi_path}/etc/apache22/server.crt" ]; then
 		/usr/bin/sed -i '' -E 's/(^commonName_max.*)/\1\
 commonName_default = ownCloud/' /etc/ssl/openssl.cnf
 	fi
-	tmp=$(mktemp /tmp/tmp.XXXXXX)
 	dd if=/dev/urandom count=16 bs=1 2> /dev/null | uuencode -|head -2 |tail -1 > "${tmp}"
 	/usr/bin/openssl req -batch -passout file:"${tmp}" -new -x509 -keyout ${owncloud_pbi_path}/etc/apache22/server.key.out -out ${owncloud_pbi_path}/etc/apache22/server.crt
 	/usr/bin/openssl rsa -passin file:"${tmp}" -in ${owncloud_pbi_path}/etc/apache22/server.key.out -out ${owncloud_pbi_path}/etc/apache22/server.key
