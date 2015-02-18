0ff0eca6d8707658f5061d06dc7002d719c49d82
Charlie Root <joshruehlig@gmail.com>
2015-02-18 14:45:21 -0800

    better grep matching, shorten system fbinary calls

--- plugins/owncloud/scripts/post-install.sh
+++ plugins/owncloud/scripts/post-install.sh
@@ -41,26 +41,26 @@ chown -R www:www ${owncloud_pbi_path}/www/owncloud media
 
 
 # Generate SSL certificate
-if [ ! -f "${owncloud_pbi_path}/etc/apache22/server.crt" ]; then
+if [ ! -f "${owncloud_pbi_path}/etc/apache24/server.crt" ]; then
 
-	if ! fgrep "commonName_default" /etc/ssl/openssl.cnf; then
-		/usr/bin/sed -i '' -E 's/(^commonName_max.*)/\1\
+	if ! grep -e '^commonName_default[[:space:]]*=' /etc/ssl/openssl.cnf; then
+		sed -i '' -E 's/(^commonName_max.*)/\1\
 commonName_default = ownCloud/' /etc/ssl/openssl.cnf
 	fi
 	dd if=/dev/urandom count=16 bs=1 2> /dev/null | uuencode -|head -2 |tail -1 > "${tmp}"
-	/usr/bin/openssl req -batch -passout file:"${tmp}" -new -x509 -keyout ${owncloud_pbi_path}/etc/apache22/server.key.out -out ${owncloud_pbi_path}/etc/apache22/server.crt
-	/usr/bin/openssl rsa -passin file:"${tmp}" -in ${owncloud_pbi_path}/etc/apache22/server.key.out -out ${owncloud_pbi_path}/etc/apache22/server.key
+	openssl req -batch -passout file:"${tmp}" -new -x509 -keyout ${owncloud_pbi_path}/etc/apache22/server.key.out -out ${owncloud_pbi_path}/etc/apache22/server.crt
+	openssl rsa -passin file:"${tmp}" -in ${owncloud_pbi_path}/etc/apache22/server.key.out -out ${owncloud_pbi_path}/etc/apache22/server.key
 
 fi
 
 #Enable SSL
-/usr/bin/sed -i '' -e 's|^#\(Include[[:space:]].*/httpd-ssl.conf$\)|\1/' ${owncloud_pbi_path}/etc/apache22/httpd.conf
+sed -i '' -e 's|^#\(Include[[:space:]].*/httpd-ssl.conf$\)|\1/' ${owncloud_pbi_path}/etc/apache22/httpd.conf
 
 #Optimize Apache on ZFS
-/usr/bin/sed -i '' -e 's/^#\(EnableMMAP[[:space:]]\).*$/\1Off/' ${owncloud_pbi_path}/etc/apache24/httpd.conf
+sed -i '' -e 's/^#\(EnableMMAP[[:space:]]\).*$/\1Off/' ${owncloud_pbi_path}/etc/apache24/httpd.conf
 
 #Enable X-Sendfile
-/usr/bin/sed -i '' -e 's/^#\(LoadModule[[:space:]]*xsendfile_module[[:space:]].*$\)/\1/' ${owncloud_pbi_path}/etc/apache24/httpd.conf
+sed -i '' -e 's/^#\(LoadModule[[:space:]]*xsendfile_module[[:space:]].*$\)/\1/' ${owncloud_pbi_path}/etc/apache24/httpd.conf
 
 #Set charset for PHP
 if [ ! -f "${owncloud_pbi_path}/etc/php.ini" ]; then
