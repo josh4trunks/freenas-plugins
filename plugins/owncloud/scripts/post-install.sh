eb4f2ccb0ef1dbfb52e1b009f9ceebcbc50944d5
Charlie Root <joshruehlig@gmail.com>
2015-02-18 00:02:29 -0800

    Add optimizations to apache

--- plugins/owncloud/scripts/post-install.sh
+++ plugins/owncloud/scripts/post-install.sh
@@ -49,4 +49,10 @@ commonName_default = ownCloud/' /etc/ssl/openssl.cnf
 fi
 
 #Enable SSL
-/usr/bin/sed -i '' -e 's|^#\(Include etc/apache24/extra/httpd-ssl.conf\)|\1/' ${owncloud_pbi_path}/etc/apache24/httpd.conf
+/usr/bin/sed -i '' -e 's|^#\(Include[[:space:]].*/httpd-ssl.conf$\)|\1/' ${owncloud_pbi_path}/etc/apache24/httpd.conf
+
+#Optimize Apache on ZFS
+/usr/bin/sed -i '' -e 's/^#\(EnableMMAP[[:space:]].*$\)/\1Off/' ${owncloud_pbi_path}/etc/apache24/httpd.conf
+
+#Enable X-Sendfile
+/usr/bin/sed -i '' -e 's/^#\(LoadModule[[:space:]]*xsendfile_module[[:space:]].*$\)/\1/' ${owncloud_pbi_path}/etc/apache24/httpd.conf
