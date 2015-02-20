55e87f3636d70443a6c2f8b0e7b3a72654b1a104
Charlie Root <joshruehlig@gmail.com>
2015-02-19 23:01:14 -0800

    Forgot to actually back it up

--- plugins/owncloud/scripts/pre-install.sh
+++ plugins/owncloud/scripts/pre-install.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 #########################################
 
+owncloud_pbi_path=/usr/pbi/owncloud-$(uname -m)
+
 # Backup ownCloud config
+mv ${owncloud_pbi_path}/www/owncloud/config/config.php /media
