c32b127817ec99579223d72cb7cfd784e45d9bc3
Charlie Root <joshruehlig@gmail.com>
2015-02-21 08:13:12 -0800

    Make menu cleaner for MB

--- plugins/mediabrowser/resources/mediabrowserUI/freenas/urls.py
+++ plugins/mediabrowser/resources/mediabrowserUI/freenas/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('mediabrowserUI.freenas.views',
-     url(r'^edit$', 'edit', name="mediabrowser_edit"),
+     url(r'^open/$', 'open_view', name="mediabrowser_open"),
      url(r'^treemenu-icon$', 'treemenu_icon', name="treemenu_icon"),
      url(r'^_s/treemenu$', 'treemenu', name="treemenu"),
      url(r'^_s/start$', 'start', name="start"),
