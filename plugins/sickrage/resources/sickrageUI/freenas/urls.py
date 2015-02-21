26ad2a402936d32a6746315185391413998d0541
Charlie Root <joshruehlig@gmail.com>
2015-02-21 07:59:09 -0800

    Make menu cleaner for SR

--- plugins/sickrage/resources/sickrageUI/freenas/urls.py
+++ plugins/sickrage/resources/sickrageUI/freenas/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('sickrageUI.freenas.views',
-     url(r'^edit$', 'edit', name="sickrage_edit"),
+     url(r'^open/$', 'open_view', name="sickrage_open"),
      url(r'^treemenu-icon$', 'treemenu_icon', name="treemenu_icon"),
      url(r'^_s/treemenu$', 'treemenu', name="treemenu"),
      url(r'^_s/start$', 'start', name="start"),
--- plugins/sickrage/resources/sickrageUI/freenas/views.py
+++ plugins/sickrage/resources/sickrageUI/freenas/views.py
@@ -251,59 +251,19 @@ def stop(request, plugin_id):
         }), content_type='application/json')
 
 
-def edit(request, plugin_id):
+def open_view(request, plugin_id):
     (sickrage_key,
     sickrage_secret) = utils.get_sickrage_oauth_creds()
     url = utils.get_rpc_url(request)
     trans = OAuthTransport(url, key=sickrage_key,
         secret=sickrage_secret)
+    server = jsonrpclib.Server(url, transport=trans)
+    jail = json.loads(server.plugins.jail.info(plugin_id))[0]
+    jail_ipv4 = jail['fields']['jail_ipv4'].split('/', 1)[0]
 
-    """
-    Get the SickRage object
-    If it does not exist create a new entry
-    """
-    try:
-        sickrage = models.SickRage.objects.order_by('-id')[0]
-    except IndexError:
-        sickrage = models.SickRage.objects.create()
-
-    try:
-        server = jsonrpclib.Server(url, transport=trans)
-        jail_path = server.plugins.jail.path(plugin_id)
-        jail = json.loads(server.plugins.jail.info(plugin_id))[0]['fields']
-        jail_ipv4 = jail['jail_ipv4'].split('/')[0]
-        auth = server.plugins.is_authenticated(
-            request.COOKIES.get("sessionid", "")
-            )
-        assert auth
-    except Exception as e:
-        raise
-
-    if request.method == "GET":
-        form = forms.SickRageForm(instance=sickrage,
-            jail_path=jail_path)
-        return render(request, "edit.html", {
-            'form': form,
-            'ipv4': jail_ipv4
-        })
-
-    if not request.POST:
-        return JsonResponse(request, error=True, message="A problem occurred.")
-
-    form = forms.SickRageForm(request.POST,
-        instance=sickrage,
-        jail_path=jail_path)
-    if form.is_valid():
-        form.save()
-
-        cmd = "%s restart" % utils.sickrage_control
-        pipe = Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE,
-            shell=True, close_fds=True)
-
-        return JsonResponse(request, error=True,
-            message="SickRage settings successfully saved.")
-
-    return JsonResponse(request, form=form)
+    return render(request, "open.html", {
+        'ipv4': jail_ipv4,
+    })
 
 
 def treemenu(request, plugin_id):
@@ -337,7 +297,7 @@ def treemenu(request, plugin_id):
         'append_to': 'plugins',
         'icon': reverse('treemenu_icon', kwargs={'plugin_id': plugin_id}),
         'type': 'pluginsfcgi',
-        'url': reverse('sickrage_edit', kwargs={'plugin_id': plugin_id}),
+        'url': reverse('sickrage_open', kwargs={'plugin_id': plugin_id}),
         'kwargs': {'plugin_name': 'sickrage', 'plugin_id': plugin_id },
     }
 
