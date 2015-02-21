2ee68683de1961ed62615372701dc64223e93896
Charlie Root <joshruehlig@gmail.com>
2015-02-21 07:56:42 -0800

    Make menu cleaner for SB

--- plugins/sickbeard/resources/sickbeardUI/freenas/urls.py
+++ plugins/sickbeard/resources/sickbeardUI/freenas/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('sickbeardUI.freenas.views',
-     url(r'^edit$', 'edit', name="sickbeard_edit"),
+     url(r'^open/$', 'open_view', name="sickbeard_open"),
      url(r'^treemenu-icon$', 'treemenu_icon', name="treemenu_icon"),
      url(r'^_s/treemenu$', 'treemenu', name="treemenu"),
      url(r'^_s/start$', 'start', name="start"),
