690f73bf2a5ecb383989839575ac2af4e6f948b2
Charlie Root <joshruehlig@gmail.com>
2015-06-27 15:38:54 -0700

    Fix bug in plex pre-install script

--- plugins/plexmediaserver/scripts/pre-install.sh
+++ plugins/plexmediaserver/scripts/pre-install.sh
@@ -6,7 +6,7 @@ plexmediaserver_pbi_path=/usr/pbi/plexmediaserver-$(uname -m)
 if [ ! -d "/var/db/plexdata" ]; then
 	if [ -d "${plexmediaserver_pbi_path}/plexdata" ]; then
 		mv "${plexmediaserver_pbi_path}/plexdata" "/var/db/plexdata"
-	elif [ -d "${plexmediaserver_pbi_path}/plexdata" ]; then
+	elif [ -d "${plexmediaserver_pbi_path}/plexdata.save" ]; then
 		mv "${plexmediaserver_pbi_path}/plexdata.save" "/var/db/plexdata"
 	fi
 fi
