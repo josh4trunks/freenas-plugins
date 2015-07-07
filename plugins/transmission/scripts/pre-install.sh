2442ea86bff3bb5e0df318ea8c3945dee14980e1
Charlie Root <joshruehlig@gmail.com>
2015-07-06 22:06:34 -0700

    Fix bug where transmission datadirectory isn't created.

--- plugins/transmission/scripts/pre-install.sh
+++ plugins/transmission/scripts/pre-install.sh
@@ -6,5 +6,7 @@ transmission_pbi_path=/usr/pbi/transmission-$(uname -m)
 if [ ! -d "/var/db/transmission" ]; then
 	if [ -d "${transmission_pbi_path}/etc/transmission/home" ]; then
 		mv "${transmission_pbi_path}/etc/transmission/home" "/var/db/transmission"
+	else
+		mkdir /var/db/transmission
 	fi
 fi
