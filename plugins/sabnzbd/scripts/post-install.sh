b60c6543115b339d8400c2dfc1b7b29258e681d7
Charlie Root <joshruehlig@gmail.com>
2015-07-18 23:16:10 -0700

    Remove Zeroconf by default

--- plugins/sabnzbd/scripts/post-install.sh
+++ plugins/sabnzbd/scripts/post-install.sh
@@ -9,8 +9,3 @@ ${sabnzbd_pbi_path}/bin/python2.7 ${sabnzbd_pbi_path}/sabnzbdUI/manage.py syncdb
 ln -s /usr/local/share/certs/ca-root-nss.crt /etc/ssl/cert.pem
 mkdir -p ${sabnzbd_pbi_path}/openssl
 ln -s /usr/local/share/certs/ca-root-nss.crt ${sabnzbd_pbi_path}/openssl/cert.pem
-
-rm ${sabnzbd_pbi_path}/etc/avahi/services/sftp-ssh.service ${sabnzbd_pbi_path}/etc/avahi/services/ssh.service
-sysrc 'dbus_enable=YES' 'avahi_daemon_enable=YES'
-${sabnzbd_pbi_path}/etc/rc.d/dbus start
-${sabnzbd_pbi_path}/etc/rc.d/avahi-daemon start
--- plugins/sabnzbd/scripts/pre-remove.sh
+++ plugins/sabnzbd/scripts/pre-remove.sh
@@ -4,5 +4,3 @@
 sabnzbd_pbi_path=/usr/pbi/sabnzbd-$(uname -m)
 
 ${sabnzbd_pbi_path}/etc/rc.d/sabnzbd forcestop 2>/dev/null || true
-${sabnzbd_pbi_path}/etc/rc.d/avahi-daemon forcestop 2>/dev/null || true
-${sabnzbd_pbi_path}/etc/rc.d/dbus forcestop 2>/dev/null || true
