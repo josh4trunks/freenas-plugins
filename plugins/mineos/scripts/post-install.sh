de926bd2543ef30c974e414bf042a32ad760fee7
Charlie Root <joshruehlig@gmail.com>
2015-08-19 20:37:01 -0700

    Update mineos to mineos-node

--- plugins/mineos/scripts/post-install.sh
+++ plugins/mineos/scripts/post-install.sh
@@ -5,13 +5,7 @@ mineos_pbi_path=/usr/pbi/mineos-$(uname -m)
 
 ${mineos_pbi_path}/bin/python2.7 ${mineos_pbi_path}/mineosUI/manage.py syncdb --migrate --noinput
 
-# Temporary workaround for a valid certificate store
-ln -s /usr/local/share/certs/ca-root-nss.crt /etc/ssl/cert.pem
-mkdir -p ${mineos_pbi_path}/openssl
-ln -s /usr/local/share/certs/ca-root-nss.crt ${mineos_pbi_path}/openssl/cert.pem
-
-mkdir -p /usr/compat/linux/proc /var/games/minecraft/profiles
-install -g mcserver /dev/null /var/games/minecraft/profiles/profile.config
+mkdir -p /usr/compat/linux/proc
 
 # Set default password only if it isn't set
 PASSWD_HASH=$(cat /etc/master.passwd | grep -e "^mcserver" | sed -e "s/^mcserver:\([^:]*\):.*$/\1/")
