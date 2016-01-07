ec776a342557fd1d7a9036bf5c06b63e6020dcd1
Charlie Root <joshruehlig@gmail.com>
2016-01-07 14:03:38 -0800

    Use Sonarr from ports

--- plugins/sonarr/scripts/post-install.sh
+++ plugins/sonarr/scripts/post-install.sh
@@ -4,3 +4,13 @@
 sonarr_pbi_path=/usr/pbi/sonarr-$(uname -m)
 
 ${sonarr_pbi_path}/bin/python2.7 ${sonarr_pbi_path}/sonarrUI/manage.py syncdb --migrate --noinput
+
+sysrc 'sonarr_data_dir=/var/db/sonarr'
+# Temporary workaround, create and set Sonarr's user as 'media'
+if [ ! -d /var/db/sonarr ]; then
+	pw useradd -n media -u 816 -d /nonexistent -s /sbin/nologin
+fi
+sysrc 'sonarr_user=media'
+
+# Allow Sonarr to update itself
+chown -R media ${sonarr_pbi_path}/share/sonarr
