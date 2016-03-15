d945e0eab0d319c2148eef331a1e7d8edff0bf5b
Charlie Root <joshruehlig@gmail.com>
2016-03-14 21:18:01 -0700

    Use upstream sabnzbd

--- plugins/sabnzbd/scripts/post-install.sh
+++ plugins/sabnzbd/scripts/post-install.sh
@@ -9,3 +9,10 @@ ${sabnzbd_pbi_path}/bin/python2.7 ${sabnzbd_pbi_path}/sabnzbdUI/manage.py syncdb
 ln -s /usr/local/share/certs/ca-root-nss.crt /etc/ssl/cert.pem
 mkdir -p ${sabnzbd_pbi_path}/openssl
 ln -s /usr/local/share/certs/ca-root-nss.crt ${sabnzbd_pbi_path}/openssl/cert.pem
+
+sysrc 'sabnzbd_conf_dir=/var/db/sabnzbd'
+# Temporary workaround, create and set Sabnzbd's user as 'media'
+if [ ! -d /var/db/sabnzbd ]; then
+	pw useradd -n media -u 816 -d /nonexistent -s /sbin/nologin
+fi
+sysrc 'sabnzbd_user=media'
