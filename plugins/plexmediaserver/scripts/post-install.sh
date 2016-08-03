6fcccff69a92aac6e5b02381cec0f88faa85b79c
Charlie Root <joshruehlig@gmail.com>
2016-08-03 13:04:24 -0700

    Fixes for old ports tree

--- plugins/plexmediaserver/scripts/post-install.sh
+++ plugins/plexmediaserver/scripts/post-install.sh
@@ -5,4 +5,12 @@ plexmediaserver_pbi_path=/usr/pbi/plexmediaserver-$(uname -m)
 
 ${plexmediaserver_pbi_path}/bin/python2.7 ${plexmediaserver_pbi_path}/plexmediaserverUI/manage.py syncdb --migrate --noinput
 
+# Temporary
 sysrc 'plexmediaserver_support_path=/var/db/plexdata'
+chmod 555 "${plexmediaserver_pbi_path}/share/plexmediaserver/Plex DLNA Server" \
+	"${plexmediaserver_pbi_path}/share/plexmediaserver/Plex Media Scanner" \
+	"${plexmediaserver_pbi_path}/share/plexmediaserver/Plex Media Server" \
+	"${plexmediaserver_pbi_path}/share/plexmediaserver/Resources/Plex Relay" \
+	"${plexmediaserver_pbi_path}/share/plexmediaserver/Resources/Plex Script Host" \
+	"${plexmediaserver_pbi_path}/share/plexmediaserver/Resources/Plex Transcoder"
+chmod 644 ${plexmediaserver_pbi_path}/share/plexmediaserver/Resources/com.plexapp.plugins.library.db	
