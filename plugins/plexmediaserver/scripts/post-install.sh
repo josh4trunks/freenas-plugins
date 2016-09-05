6f0e6110ce173fe1c70398116f2a281fd4806dcc
Charlie Root <joshruehlig@gmail.com>
2016-09-04 21:15:46 -0700

    Update plex

--- plugins/plexmediaserver/scripts/post-install.sh
+++ plugins/plexmediaserver/scripts/post-install.sh
@@ -10,7 +10,7 @@ sysrc 'plexmediaserver_support_path=/var/db/plexdata'
 chmod 555 "${plexmediaserver_pbi_path}/share/plexmediaserver/Plex DLNA Server" \
 	"${plexmediaserver_pbi_path}/share/plexmediaserver/Plex Media Scanner" \
 	"${plexmediaserver_pbi_path}/share/plexmediaserver/Plex Media Server" \
-	"${plexmediaserver_pbi_path}/share/plexmediaserver/Resources/Plex Relay" \
-	"${plexmediaserver_pbi_path}/share/plexmediaserver/Resources/Plex Script Host" \
-	"${plexmediaserver_pbi_path}/share/plexmediaserver/Resources/Plex Transcoder"
+	"${plexmediaserver_pbi_path}/share/plexmediaserver/Plex Relay" \
+	"${plexmediaserver_pbi_path}/share/plexmediaserver/Plex Script Host" \
+	"${plexmediaserver_pbi_path}/share/plexmediaserver/Plex Transcoder"
 chmod 644 ${plexmediaserver_pbi_path}/share/plexmediaserver/Resources/com.plexapp.plugins.library.db	
