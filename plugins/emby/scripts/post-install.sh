78dcb37a8828df37dc58c51ee5e97ef41ca5f5bf
Charlie Root <joshruehlig@gmail.com>
2017-04-22 19:13:30 -0700

    Add font config for emby

--- plugins/emby/scripts/post-install.sh
+++ plugins/emby/scripts/post-install.sh
@@ -4,3 +4,14 @@
 emby_pbi_path=/usr/pbi/emby-$(uname -m)
 
 ${emby_pbi_path}/bin/python2.7 ${emby_pbi_path}/embyUI/manage.py syncdb --migrate --noinput
+
+# Install fonts config if it doesn't exist
+if [ ! -f /usr/local/etc/fonts/fonts.conf ]; then
+	mkdir -p /usr/local/etc/fonts
+	cat << __EOF__ > /usr/local/etc/fonts/fonts.conf
+<?xml version="1.0"?>
+<fontconfig>
+	<dir>/usr/local/lib/X11/fonts</dir>
+</fontconfig>
+__EOF__
+fi
